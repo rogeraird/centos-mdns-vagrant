b283f4e87309dc24400fadec10be9a8c8a3441f1
Roger Aird <Roger Aird>
2016-07-28 18:14:53 +0100

    Fix case of mDNS in bootstrap.sh comments

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,13 +4,13 @@ yum update; yum upgrade -y
 
 echo gitlab.local > /etc/hostname
 
-# Enable mdns, so that ansible can access the box by hostname
+# Enable mDNS, so that ansible can access the box by hostname
 curl http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-7.noarch.rpm -o epel-release-7-7.noarch.rpm
 rpm -ivh epel-release-7-7.noarch.rpm
 rm epel-release-7-7.noarch.rpm
 yum install -y nss-mdns avahi
 
-# Allow UDP traffic to mdns port
+# Allow UDP traffic to mDNS port
 iptables -A INPUT -p udp -m udp --dport 5353 -j ACCEPT
 service avahi-daemon start
 
